edfb3190f307b3eb819c96becf23104462de138b
HarikaYarlagadda <hariyhari1999@gmail.com>
2020-10-22 23:33:51 +0530

    sorted the array in descending order

--- ArthematicComputation.sh
+++ ArthematicComputation.sh
@@ -17,3 +17,27 @@ operations[2]=$operation2
 operations[3]=$operation3
 operations[4]=$operation4
 echo ${operations[@]}
+variable=${operations[@]}
+for values in $variable
+do
+	array[count]=$values
+	count=$(( $count+1 ))
+done
+echo "array values" ${array[@]}
+for (( i=0 ; i < ${#array[@]}; i++ )) 
+do
+    for (( j=0 ; j < ${#array[@]}; j++ )) 
+    do
+      if [[ ${array[$j]} -lt  ${array[$i]} ]]
+      then
+        tmp=${array[$i]}
+        array[$i]=${array[$j]}
+        array[$j]=${tmp}
+      fi
+    done
+done
+
+for n in "${array[@]}"
+do
+    echo "$n"
+done
