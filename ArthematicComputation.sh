70472e85ae63cc2bd1d53508c8f64af105e1c6ba
HarikaYarlagadda <hariyhari1999@gmail.com>
2020-10-22 22:46:47 +0530

    done arthematic operation2

--- ArthematicComputation.sh
+++ ArthematicComputation.sh
@@ -4,3 +4,5 @@ read -p "enter second number:" number2
 read -p "enter third number:" number3
 operation1=$(( $number1+$number2*$number3 ))
 echo $operation1
+operation2=$(( $number1*$number2+$number3 ))
+echo $operation2
